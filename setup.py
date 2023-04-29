52ec91572d6b0bd33e19f3600cbfbe9afe6ba752
Joseph Farrell <disastrousgamemaster@gmail.com>
2023-04-28 21:32:46 -0700

    add setup.py and cfg

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'atppy',
+  packages = ['atppy'],
+  version = '0.1',
+  license='MIT',
+  description = 'Python library for AT Protocol',
+  author = 'Joseph Farrell',
+  author_email = 'joe.farrell373@gmail.com',
+  url = 'https://github.com/Gladdstone/atppy',
+  download_url = 'https://github.com/Gladdstone/atppy/archive/v_01.tar.gz',
+  keywords = ['at protocol', 'bluesky', 'bsky'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
