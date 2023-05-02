51b05ec2f2b140edc4333c5de437ccc431271c9e
Joseph Farrell <disastrousgamemaster@gmail.com>
2023-05-02 11:21:00 -0700

    add getPopular endpoint

--- atppy/lib/bsky/__init__.py
+++ atppy/lib/bsky/__init__.py
@@ -2,3 +2,4 @@ from atppy.lib.bsky.Actor import Actor
 from atppy.lib.bsky.Feed import Feed
 from atppy.lib.bsky.Graph import Graph
 from atppy.lib.bsky.Notification import Notification
+from atppy.lib.bsky.Unspecced import Unspecced
